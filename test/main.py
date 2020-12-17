f5c48b660234e89a29299932d2757e4545d7b114
Hunter Negron <hnegron@hawk.iit.edu>
2020-12-17 15:54:56 -0600

    basic queue test

--- test/main.py
+++ test/main.py
@@ -12,28 +12,44 @@ types = ["A", "B", "C"]
 def dripper():
     '''enqueues one or more until finished'''
     for i in range(20):
-        q.append(datum(types[random.randint(0, 2)], i))
+        yield datum(types[random.randint(0, 2)], i)
+
+x = dripper()
+done = False
 
 while True:
-
+    if done:
+        print("exiting")
+        break
+    print("{")
+    try:
+        for i in range(random.randint(1, 5)):
+            q.append(next(x))
+    except Exception as e:
+        print("done set to true")
+        done = True
 
     while True:
+        print("  [")
         event = None
         if len(q) > 0:
             event = q.pop(0)
         else:
-            print("queue empty. exiting")
+            print("    queue empty.")
+            print("  ]")
             break
 
         if event == None:
-            print("Invalid event")
+            print("    Invalid event")
         elif event.the_type == "A":
-            print("Type A with info = {}".format(event.the_info))
+            print("    Type A with info = {}".format(event.the_info))
         elif event.the_type == "B":
-            print("Type B with info = {}".format(event.the_info))
+            print("    Type B with info = {}".format(event.the_info))
         elif event.the_type == "C":
-            print("Type C with info = {}".format(event.the_info))
+            print("    Type C with info = {}".format(event.the_info))
         else:
-            print("Invalid type")
+            print("    Invalid type")
 
-    print()
+        print("  ]")
+
+    print("}\n")
